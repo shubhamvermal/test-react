ecc0f09f6151f5de4ed003a43e56dcdbbd92a10b
mr-verma1024 <shubhamverma1024@gmail.com>
2020-04-18 22:11:07 +0530

    Updates

---
+++ precache-manifest.50d338a50ea79f6bfc1e63eb17c8e0b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5687f55aa851cafe945b0bfc3538c0f9",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "b7eafbd1992736e1558c",
+    "url": "/react-test/static/css/main.eb5a0b6c.chunk.css"
+  },
+  {
+    "revision": "7ccea09683d2b8a074ef",
+    "url": "/react-test/static/js/2.9dfaa321.chunk.js"
+  },
+  {
+    "revision": "b7eafbd1992736e1558c",
+    "url": "/react-test/static/js/main.b34bcee5.chunk.js"
+  },
+  {
+    "revision": "f6ca8a104cc1fb226dd8",
+    "url": "/react-test/static/js/runtime-main.0cae5efe.js"
+  }
+]);
